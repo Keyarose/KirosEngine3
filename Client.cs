54b43f0e612d22ece39a734a523512220154196a
bgillcash <myst600@hotmail.com>
2024-01-14 20:39:08 -0400

    Client class abstract and basic logging system.

---
+++ Client.cs
@@ -0,0 +1,67 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirosEngine3
+{
+    /// <summary>
+    /// Abstract class to be inherited by indivdual works using the engine
+    /// </summary>
+    public abstract class Client : GameWindow
+    {
+        public Client(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { ClientSize = (width, height), Title = title })
+        {
+
+        }
+
+        /// <summary>
+        /// Load any data needed for the inital screen
+        /// </summary>
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        /// <summary>
+        /// Update data before the draw of the next frame
+        /// </summary>
+        /// <param name="args">Frame event arguments</param>
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        /// <summary>
+        /// Render the frame, making draw calls to any objects to be drawn
+        /// </summary>
+        /// <param name="args">Frame event arguemnts</param>
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+        }
+
+        /// <summary>
+        /// Handle window resize events
+        /// </summary>
+        /// <param name="e">Resize event arguments</param>
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        /// <summary>
+        /// Cleanup and unload items before closing the program
+        /// </summary>
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirosEngine3
+{
+    /// <summary>
+    /// Writes information to a dated log file during application activity.
+    /// </summary>
+    public class Logger
+    {
+        protected static Logger? _instance;
+
+        protected string _filePath = "log/"; //default filepath
+        protected FileInfo? _fileInfo;
+        protected int _maxLogSizeMB = 512;
+        protected int _maxLogSizeL = 10000; //max log size in lines
+
+        protected int _currentLineCount = 0;
+
+        /// <summary>
+        /// Accessor for the logger instance
+        /// </summary>
+        public static Logger Instance
+        {
+            get
+            {
+                _instance ??= new Logger();
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// The maximum logsize in MB
+        /// </summary>
+        public int MaxLogSizeMB
+        {
+            get { return _maxLogSizeMB; }
+        }
+
+        /// <summary>
+        /// The maximum logsize in lines
+        /// </summary>
+        public int MaxLogSizeL
+        {
+            get { return _maxLogSizeL; }
+        }
+
+        /// <summary>
+        /// The number of lines currently in the log
+        /// </summary>
+        public int LineCount
+        {
+            get { return _currentLineCount; }
+        }
+
+        /// <summary>
+        /// Logger initialization, called only once 
+        /// </summary>
+        protected Logger() 
+        {
+            DateTime now = DateTime.Now;
+
+            //default file path and name
+            _filePath += string.Format("eventlog_{0}.log", now.ToString("MM/dd/yyyy-HH-mm-ss-fff"));
+
+            //todo: load max log sizes from config file
+
+            try
+            {
+                _fileInfo = new FileInfo(_filePath);
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                //todo: write to in game console
+            }
+
+            //if fileinfo is not null continue log setup else write an error to the consoles
+            if (_fileInfo != null)
+            {
+                _fileInfo.Directory?.Create();
+
+                if(!_fileInfo.Exists)
+                {
+                    try
+                    {
+                        //create the file and write intial data to it
+                        using StreamWriter sw = _fileInfo.CreateText();
+                        sw.WriteLine(string.Format("Log File for: {0}", now.ToString("MM/dd/yyyy-HH:mm:ss.fff")));
+                        sw.WriteLine("Log Init");
+
+                        _currentLineCount = 2;
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        //todo: write to in game console
+                    }
+                }
+                else
+                {
+                    //the file already existed, so append rather than overwrite after getting the line count
+                    _currentLineCount = File.ReadLines(_fileInfo.FullName).Count();
+
+                    using StreamWriter sw = _fileInfo.AppendText();
+                    sw.WriteLine(string.Format("Log Reinitialized at: {0}", DateTime.Now.ToString("MM/dd/yyyy-HH:mm:ss.fff")));
+                    _currentLineCount++;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Failure to setup the log file, see the last exception message.");
+                //todo: write to in game console
+            }
+        }
+
+        /// <summary>
+        /// Write a string to the log file
+        /// </summary>
+        /// <param name="message">The string to write</param>
+        public void WriteToLog(string message)
+        {
+            if(_fileInfo != null)
+            {
+                if(!IsLogFull())
+                {
+                    try
+                    {
+                        //append the message to the log prefixed by the datetime to milisecond
+                        using StreamWriter sw = _fileInfo.AppendText();
+                        sw.WriteLine(string.Format("{0} : {1}", DateTime.Now.ToString("MM/dd/yy-HH:mm:ss.fff"), message));
+                        _currentLineCount++;
+                    }
+                    catch (IOException ex) 
+                    {
+                        Console.WriteLine(ex.ToString());
+                        //todo: write to in game console
+                    }
+                }
+                else
+                {
+                    BeginNewLog();
+
+                    try
+                    {
+                        //append the message to the log prefixed by the datetime to milisecond
+                        using StreamWriter sw = _fileInfo.AppendText();
+                        sw.WriteLine(string.Format("{0} : {1}", DateTime.Now.ToString("MM/dd/yy-HH:mm:ss.fff"), message));
+                        _currentLineCount++;
+                    }
+                    catch (IOException ex)
+                    {
+                        Console.WriteLine(ex.ToString());
+                        //todo: write to in game console
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a new log file for the current date time and set it to be used. Called when the current log is full.
+        /// </summary>
+        protected void BeginNewLog()
+        {
+            DateTime now = DateTime.Now;
+
+            _filePath = "log/" + string.Format("eventlog_{0}.log", now.ToString("MM/dd/yyyy-HH-mm-ss-fff"));
+
+            try
+            {
+                _fileInfo = new FileInfo(_filePath);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                //todo: write to in game console
+            }
+
+            if(_fileInfo != null)
+            {
+                try
+                {
+                    //create the file and write intial data to it
+                    using StreamWriter sw = _fileInfo.CreateText();
+                    sw.WriteLine(string.Format("Log File for: {0}", now.ToString("MM/dd/yyyy-HH:mm:ss.fff")));
+                    sw.WriteLine("Log Init");
+
+                    _currentLineCount = 2;
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                    //todo: write to in game console
+                }
+            }
+        }
+
+        /// <summary>
+        /// Check that the log file is not larger than allowed.
+        /// </summary>
+        /// <returns>True if the file is full or doesn't exist, false if there is room to write to</returns>
+        protected bool IsLogFull()
+        {
+            if(_fileInfo != null)
+            {
+                if(_currentLineCount < _maxLogSizeL && _fileInfo.Length / 1000000 < _maxLogSizeMB)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            //the file doesn't exist so it can't be written to anyway
+            return true;
+        }
+    }
+}
