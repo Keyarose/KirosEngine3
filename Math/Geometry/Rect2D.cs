d8ace29fb83c3686d0947d4b034125eb6063a59b
bgillcash <myst600@hotmail.com>
2024-03-29 15:42:43 -0300

    Comparison and string methods for Rect2D, Triangle2D, and special points for Triangle2D.

--- Math/Geometry/Rect2D.cs
+++ Math/Geometry/Rect2D.cs
@@ -1,6 +1,7 @@
 ﻿using KirosEngine3.Math.Vector;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace KirosEngine3.Math.Geometry
     /// <summary>
     /// Define a 2D rectangle
     /// </summary>
-    public struct Rect2D
+    public struct Rect2D : IEquatable<Rect2D>, IFormattable, IGeometrical
     {
         /// <summary>
         /// The rectangle's vertices defined in clockwise order starting from the upper left
@@ -23,29 +24,29 @@ namespace KirosEngine3.Math.Geometry
         }
 
         /// <summary>
-        /// Top edge of the rectangle
+        /// Edge of the rectangle formed by Vertex 0 and 1
         /// </summary>
         public readonly Vec2 Edge1 { get { return Vertices[1] - Vertices[0]; } }
 
         /// <summary>
-        /// Right edge of the rectangle
+        /// Edge of the rectangle formed by Vertex 1 and 2
         /// </summary>
         public readonly Vec2 Edge2 { get { return Vertices[2] - Vertices[1]; } }
 
         /// <summary>
-        /// Bottom edge of the rectangle
+        /// Edge of the rectangle formed by Vertex 2 and 3
         /// </summary>
-        public readonly Vec2 Edge3 { get { return Vertices[2] - Vertices[3]; } }
+        public readonly Vec2 Edge3 { get { return Vertices[3] - Vertices[2]; } }
 
         /// <summary>
-        /// Left edge of the rectangle
+        /// Edge of the rectangle formed by Vertex 3 and 0
         /// </summary>
-        public readonly Vec2 Edge4 { get { return Vertices[3] - Vertices[0]; } }
+        public readonly Vec2 Edge4 { get { return Vertices[0] - Vertices[3]; } }
 
         /// <summary>
         /// The perimeter of the rectangle
         /// </summary>
-        public readonly float Perimeter { get { return Edge1.Length + Edge2.Length + Edge3.Length + Edge4.Length; } }
+        public readonly float Perimeter { get { return Edge1.Length * 2 + Edge2.Length * 2; } }
 
         /// <summary>
         /// The area of the rectangle
@@ -113,5 +114,118 @@ namespace KirosEngine3.Math.Geometry
 
             return false;
         }
+
+        /// <summary>
+        /// Get the rectangle as an array of 2D triangles
+        /// </summary>
+        /// <param name="split02">The vertices to split along; True splits along vertex 0 and 2, false 1 and 3</param>
+        /// <returns>The rectangle as an array of triangles</returns>
+        public readonly Triangle2D[] AsTriangles(bool split02)
+        {
+            Triangle2D[] result = new Triangle2D[2];
+
+            if (split02) 
+            {
+                result[0] = new Triangle2D(Vertices[..3]);//verts 0,1,2
+                result[1] = new Triangle2D([Vertices[0], Vertices[2], Vertices[3]]);//verts 0,2,3
+            }
+            else
+            {
+                result[0] = new Triangle2D([Vertices[0], Vertices[1], Vertices[3]]);//verts 0,1,3
+                result[1] = new Triangle2D(Vertices[1..4]);//verts 1,2,3
+            }
+
+            return result;
+        }
+
+        /// <inheritdoc/>
+        public override readonly bool Equals([NotNullWhen(true)] object? obj)
+        {
+            return obj is Rect2D rec && Equals(rec);
+        }
+
+        /// <inheritdoc/>
+        public readonly bool Equals(Rect2D other)
+        {
+            return Vertices.SequenceEqual(other.Vertices);
+        }
+
+        /// <inheritdoc/>
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(Vertices);
+        }
+
+        /// <summary>
+        /// Define the equivalence operator for a 2D rectangle
+        /// </summary>
+        /// <param name="lhs">The left value</param>
+        /// <param name="rhs">The right value</param>
+        /// <returns>True if they are equal false otherwise</returns>
+        public static bool operator ==(Rect2D lhs, Rect2D rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Define the non-equivalence operator for a 2D rectangle
+        /// </summary>
+        /// <param name="lhs">The left value</param>
+        /// <param name="rhs">The right value</param>
+        /// <returns>False if they are equal, true otherwise</returns>
+        public static bool operator !=(Rect2D lhs, Rect2D rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        /// <inheritdoc/>
+        public readonly bool IsGeometricallyCorrect(out string? message)
+        {
+            //check that edges are parallel or perp as needed
+            if (!Edge1.IsParallel(Edge3) || !Edge2.IsParallel(Edge4))
+            {
+                message = "Rectangle is not geometrically correct as the edges are not parallel to their opposites.";
+                return false;
+            }
+
+            if (Edge1.Length != Edge3.Length || Edge2.Length != Edge4.Length)
+            {
+                message = "Rectangle is not geometrically correct as the opposite edges are different sizes."; ;
+                return false;
+            }
+
+            message = null;
+            return true;
+        }
+
+        #region ToString
+        /// <inheritdoc/>
+        public override readonly string ToString()
+        {
+            return ToString(null, null);
+        }
+
+        /// <inheritdoc cref="ToString(string?, IFormatProvider?)"/>
+        public readonly string ToString(string? format)
+        {
+            return ToString(format, null);
+        }
+
+        /// <inheritdoc cref="ToString(string?, IFormatProvider?)"/>
+        public readonly string ToString(IFormatProvider? formatProvider)
+        {
+            return ToString(null, formatProvider);
+        }
+
+        /// <inheritdoc/>
+        public readonly string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return string.Format("Rect3D \n\tVertices: {0}, \n\t\t{1}, \n\t\t{2}, \n\t\t{3}",
+                Vertices[0].ToString(format, formatProvider),
+                Vertices[1].ToString(format, formatProvider),
+                Vertices[2].ToString(format, formatProvider),
+                Vertices[3].ToString(format, formatProvider));
+        }
+        #endregion
     }
 }
--- Math/Geometry/Triangle2D.cs
+++ Math/Geometry/Triangle2D.cs
@@ -1,6 +1,7 @@
 ﻿using KirosEngine3.Math.Vector;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace KirosEngine3.Math.Geometry
     /// <summary>
     /// Define a 2D triangle
     /// </summary>
-    public struct Triangle2D
+    public struct Triangle2D : IEquatable<Triangle2D>, IFormattable
     {
         public Vec2[] Vertices = new Vec2[3];
         
@@ -37,6 +38,19 @@ namespace KirosEngine3.Math.Geometry
         public readonly Vec2 Edge3 { get { return Vertices[1] - Vertices[0]; } }
 
         /// <summary>
+        /// The angle of the triangle at Vertex 0
+        /// </summary>
+        public readonly float Angle1 { get { return MathF.Acos(Vec2.Dot(Edge2, Edge3) / (Edge2.Length * Edge3.Length)); } }
+        /// <summary>
+        /// The angle of the triangle at Vertex 1
+        /// </summary>
+        public readonly float Angle2 { get { return MathF.Acos(Vec2.Dot(Edge1, Edge3) / (Edge1.Length * Edge3.Length)); } }
+        /// <summary>
+        /// The angle of the triangle at Vertex 2
+        /// </summary>
+        public readonly float Angle3 { get { return MathF.Acos(Vec2.Dot(Edge1, Edge2) / (Edge1.Length * Edge2.Length)); } }
+
+        /// <summary>
         /// The perimeter of the triangle
         /// </summary>
         public readonly float Perimeter { get { return Edge1.Length + Edge2.Length + Edge3.Length; } }
@@ -46,9 +60,171 @@ namespace KirosEngine3.Math.Geometry
         /// </summary>
         public readonly float Area { get { return Vec3.Cross(new Vec3(Edge1, 0.0f), new Vec3(Edge2, 0.0f)).Length / 2; } }
 
+        /// <inheritdoc/>
+        public readonly bool Equals(Triangle2D other)
+        {
+            if (other.Vertices.Contains(Vertices[0]) && other.Vertices.Contains(Vertices[1]) && other.Vertices.Contains(Vertices[2]))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc/>
+        public override readonly bool Equals([NotNullWhen(true)] object? obj)
+        {
+            return obj is Triangle2D t && Equals(t);
+        }
+
+        /// <inheritdoc/>
+        public override readonly int GetHashCode()
+        {
+            return HashCode.Combine(Vertices);
+        }
+
+        /// <summary>
+        /// Define the equivalence operator between two 2D triangles
+        /// </summary>
+        /// <param name="lhs">The left value</param>
+        /// <param name="rhs">The right value</param>
+        /// <returns>True if the triangles are equal, false otherwise</returns>
+        public static bool operator ==(Triangle2D lhs, Triangle2D rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Define the non-equivalence operator between two 2D triangles
+        /// </summary>
+        /// <param name="lhs">The left value</param>
+        /// <param name="rhs">The right value</param>
+        /// <returns>False if the triangles are equal, true otherwise</returns>
+        public static bool operator !=(Triangle2D lhs, Triangle2D rhs)
+        {
+            return !lhs.Equals(rhs);
+        }
+
+        /// <summary>
+        /// Check if the given point is inside the triangle
+        /// </summary>
+        /// <param name="point">The point to check</param>
+        /// <returns>True if the point is inside the triangle, false otherwise</returns>
         public readonly bool ContainsPoint(Vec2 point)
         {
-            throw new NotImplementedException();//TODO:implement
+            Vec3 baryCoords = BarycentricCoords(point);
+
+            if (baryCoords.X < 0.0f || baryCoords.Y < 0.0f || baryCoords.Z < 0.0f)
+            {
+                return false;
+            }
+
+            return true;
         }
+
+        /// <summary>
+        /// Calculate the barycentric coordinates for the given point
+        /// </summary>
+        /// <param name="point">The point to calculate for</param>
+        /// <returns>The barycentric version of the point</returns>
+        public readonly Vec3 BarycentricCoords(Vec2 point) 
+        {
+            Triangle2D subT1 = new Triangle2D([Vertices[1], Vertices[2], point]);//sub-triangle on edge 1
+            Triangle2D subT2 = new Triangle2D([Vertices[2], Vertices[0], point]);//sub-triangle on edge 2
+
+            float area = Area;//cache locally so it is only calculated once
+
+            float x = subT1.Area / area;
+            float y = subT2.Area / area;
+
+            return new Vec3(x, y, 1.0f - x - y);
+        }
+
+        /// <summary>
+        /// The center of gravity for the triangle
+        /// </summary>
+        /// <returns>A 2D point representing the center of gravity</returns>
+        public readonly Vec2 CenterOfGrav()
+        {
+            return (Vertices[0] + Vertices[1] + Vertices[2]) / 3.0f;
+        }
+
+        /// <summary>
+        /// The incenter of the triangle
+        /// </summary>
+        /// <returns>A 2D point representing the triangle's incenter</returns>
+        public readonly Vec2 Incenter()
+        {
+            return (Vertices.Length * Vertices[0] + Vertices.Length * Vertices[1] + Vertices.Length * Vertices[2]) / Perimeter;
+        }
+
+        /// <summary>
+        /// The circle that fits within the triangle at the incenter
+        /// </summary>
+        /// <returns>A 2D circle</returns>
+        public readonly Circle2D IncenterCircle()
+        {
+            return new Circle2D(Area / Perimeter, Incenter());
+        }
+
+        /// <summary>
+        /// The circumcenter, the center of the circle that fits the vertices of the triangle
+        /// </summary>
+        /// <param name="circumradius">The radius of the circle</param>
+        /// <returns>A 2D point representing the circumcenter</returns>
+        public readonly Vec2 Circumcenter(out float circumradius)
+        {
+            float d1 = Vec2.Dot(-Edge2, Edge3);
+            float d2 = Vec2.Dot(-Edge3, Edge1);
+            float d3 = Vec2.Dot(-Edge1, Edge2);
+
+            float c1 = d2 * d3;
+            float c2 = d1 * d3;
+            float c3 = d2 * d1;
+            float c = c1 + c2 + c3;
+
+            circumradius = MathF.Sqrt((d1 + d2) * (d2 + d3) * (d3 + d1) / c) / 2.0f;
+
+            return (((c2 + c3) * Vertices[0]) + ((c3 + c1) * Vertices[1]) + ((c1 + c2) * Vertices[2])) / (2.0f * c);
+        }
+
+        /// <summary>
+        /// The circle that fits all three of the triangle's vertices
+        /// </summary>
+        /// <returns>A 2D circle</returns>
+        public readonly Circle2D CircumcenterCircle()
+        {
+            Vec2 center = Circumcenter(out float radius);
+            return new Circle2D(radius, center);
+        }
+
+        #region ToString
+        /// <inheritdoc/>
+        public override readonly string ToString()
+        {
+            return ToString(null, null);
+        }
+
+        /// <inheritdoc cref="ToString(string?, IFormatProvider?)"/>
+        public readonly string ToString(string? format)
+        {
+            return ToString(format, null);
+        }
+
+        /// <inheritdoc cref="ToString(string?, IFormatProvider?)"/>
+        public readonly string ToString(IFormatProvider? formatProvider)
+        {
+            return ToString(null, formatProvider);
+        }
+
+        /// <inheritdoc/>
+        public readonly string ToString(string? format, IFormatProvider? formatProvider)
+        {
+            return string.Format("Triangle2D \n\tVertices: {0}, \n\t\t{1}, \n\t\t{2}",
+                Vertices[0].ToString(format, formatProvider),
+                Vertices[1].ToString(format, formatProvider),
+                Vertices[2].ToString(format, formatProvider));
+        }
+        #endregion
     }
 }
