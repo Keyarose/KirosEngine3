70f03fca53367d45acd93a215f32a2066336969e
bgillcash <myst600@hotmail.com>
2024-06-10 18:43:39 -0300

    Initial Material and MaterialManager.

---
+++ Materials/Material.cs
@@ -0,0 +1,155 @@
+﻿using KirosEngine3.Math.Data;
+using KirosEngine3.Mesh;
+using KirosEngine3.Shaders;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace KirosEngine3.Materials
+{
+    /// <summary>
+    /// Defines a Material to be applied to a renderable object.
+    /// </summary>
+    public class Material : IXmlSerializable
+    {
+        protected string _name = "";
+        protected string _description = "";
+
+        protected Color4 _ambientColor;
+        protected Color4 _diffuseColor;
+        protected Color4 _specularColor;
+        protected float _shininess;
+
+        protected string[] _textures = [];
+
+        protected string _shaderName = "";
+
+        /// <summary>
+        /// The name of the Material.
+        /// </summary>
+        public string Name { get { return _name; } set { _name = value; } }
+
+        /// <summary>
+        /// The description of the Material.
+        /// </summary>
+        public string Description { get { return _description; } set { _description = value; } }
+
+        /// <summary>
+        /// The ambient color of the Material.
+        /// </summary>
+        public Color4 AmbientColor { get { return _ambientColor; } set { _ambientColor = value; } }
+
+        /// <summary>
+        /// The diffuse color of the Material.
+        /// </summary>
+        public Color4 DiffuseColor { get { return _diffuseColor; } set { _diffuseColor = value; } }
+
+        /// <summary>
+        /// The specular color of the Material.
+        /// </summary>
+        public Color4 SpecularColor { get { return _specularColor; } set { _specularColor = value; } }
+
+        /// <summary>
+        /// The shininess of the Material.
+        /// </summary>
+        public float Shininess { get { return _shininess; } set { _shininess = value; } }
+
+        /// <summary>
+        /// The name of the shader to be used by the Material.
+        /// </summary>
+        public string ShaderName { get { return _shaderName; } set { _shaderName = value; } }
+
+        public Material() { }
+
+        #region Load
+        public bool SetShaderAttrib(ShaderAttribFlags attribFlag, ShaderAttribSettings settings)
+        {
+            if (!ShaderManager.TryGetShader(_shaderName, out Shader? sh))
+            {
+                Logger.WriteToLog("Material: {0}, references missing shader named: {1}", this, _shaderName);
+                Console.WriteLine("Material: {0}, references missing shader named: {1}", this, _shaderName);
+
+                return false;
+            }
+
+            if (attribFlag.HasFlag(ShaderAttribFlags.Position))
+            {
+                sh.SetPositionAttribGL(settings);
+            }
+            else if (attribFlag.HasFlag(ShaderAttribFlags.Normal))
+            {
+                //sh.SetNormalAttribGL(settings);
+            }
+            else if (attribFlag.HasFlag(ShaderAttribFlags.Color))
+            {
+                sh.SetColorAttribGL(settings);
+            }
+            else if (attribFlag.HasFlag(ShaderAttribFlags.UV))
+            {
+                sh.SetUVAttribGL(settings);
+            }
+
+            return true;
+        }
+
+        public bool SetCustomShaderAttrib(ShaderAttribSettings settings)
+        {
+            if (!ShaderManager.TryGetShader(_shaderName, out Shader? sh))
+            {
+                Logger.WriteToLog("Material: {0}, references missing shader named: {1}", this, _shaderName);
+                Console.WriteLine("Material: {0}, references missing shader named: {1}", this, _shaderName);
+
+                return false;
+            }
+
+            sh.SetAttribGL(settings);
+
+            return true;
+        }
+        #endregion
+
+        #region Draw
+        /// <summary>
+        /// Apply the material to the caller.
+        /// </summary>
+        /// <param name="vm">The view matrices for the object the material is being applied to.</param>
+        /// <returns>True if successfully applied, false otherwise.</returns>
+        public bool Apply(ViewMatrixes vm)
+        {
+            if (!ShaderManager.TryGetShader(_shaderName, out Shader? sh))
+            {
+                Logger.WriteToLog("Material: {0}, references missing shader named: {1}", this, _shaderName);
+                Console.WriteLine("Material: {0}, references missing shader named: {1}", this, _shaderName);
+
+                return false;
+            }
+
+            sh.UseGL();
+
+            //set uniforms
+
+            return true;
+        }
+        #endregion
+
+        #region Xml
+        /// <inheritdoc/>
+        public XmlSchema? GetSchema()
+        {
+            return null;
+        }
+
+        /// <inheritdoc/>
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc/>
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Materials/MaterialManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirosEngine3.Materials
+{
+    internal class MaterialManager
+    {
+    }
+}
